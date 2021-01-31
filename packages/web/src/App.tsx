30fa6baa9f773ddda15011efc2d357220c2c5a19
Petr Vecera <petr.vecera@profiq.com>
2021-01-31 22:38:51 +0100

    Add antd

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
+import { Button } from 'antd';
 import './App.css';
 
-function App() {
-  return (
+const App: FC = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button type="primary">Button</Button>
     </div>
-  );
-}
+);
 
 export default App;
