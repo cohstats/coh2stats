9581b68a3c4a8d292c804c6ce722a9ada9ba3d26
rempAut <80215310+rempAut@users.noreply.github.com>
2021-03-08 21:34:48 +0100

    commander ant design playground (#1)

--- packages/web/src/components/wip-ramp/commanders.tsx
+++ packages/web/src/components/wip-ramp/commanders.tsx
@@ -1,28 +1,121 @@
 import React from "react";
-import {Card, Col, Row} from "antd";
-
+import { Card, Col, Row, Image, List, Divider, Avatar, Descriptions, Badge } from "antd";
+import background from "/resources/commanderImage/placeholder.svg";
+import { ClockCircleOutlined } from "@ant-design/icons";
 export const RampComponent = () => {
+    let myData = {
+        serverID: "186415",
+        commanderName: "Armor Company",
+        description:
+            "Overwhelm the enemy with elite American armored vehicles and supporting infantry. Assault engineers can clear fortifications to allow vehicles to advance, while Sherman Bulldozers can be used to setup a roadblock or defensive line. The enemy\u2019s strongpoints can be bombarded with 240mm artillery prior to an attack.",
+        races: ["usf"],
+        abilities: [
+            {
+                reference: "commander_ability\\aef\\commander\\ability\\m10_deploy_clone",
+                name: "M10 'Wolverine' Tank Destroyer",
+                description:
+                    "Deploy an M10 Tank Destroyer. The 'Wolverine's'' 3 inch main gun is effective against all but the heaviest enemy armored vehicles. Effective vs tanks and vehicles.",
+            },
+            {
+                reference:
+                    "commander_ability\\aef\\commander\\ability\\sherman_bulldozer_dispatch",
+                name: "105mm Bulldozer Sherman",
+                description:
+                    "An M4A3 Sherman Bulldozer tank equipped with a 105mm howitzer for engaging infantry and structures can be called into the battlefield.",
+            },
+            {
+                reference:
+                    "commander_ability\\aef\\commander\\ability\\assault_engineer_dispatch",
+                name: "Assault Engineers",
+                description:
+                    "An Assault Engineer Squad with short range sub machine guns can be ordered in to the battlefield.",
+            },
+            {
+                reference: "commander_ability\\aef\\commander\\ability\\siege_240mm_artillery",
+                name: "240mm Howitzer Barrage",
+                description: "Call in a 240mm Howitzer barrage to level the target area.",
+            },
+            {
+                reference: "commander_ability\\aef\\commander\\passive\\elite_vehicle_crews",
+                name: "Elite Vehicle Crews Upgrade",
+                description:
+                    "Vehicle crews are upgraded with Thompson submachine guns, increased repair speeds and earn veterancy at an increased rate.",
+            },
+        ],
+    };
+
+    function ImageDemo() {
+        return (
+            <Image width={150} preview={false} src="/resources/commanderImage/placeholder.svg" />
+        );
+    }
+
+    function ImageRace() {
+        if ((myData.races[0] = "usf")) {
+            return <Image width={350} preview={false} src="/resources/us-forces.png" />;
+        } else {
+            return (
+                <Image
+                    width={150}
+                    preview={false}
+                    src="/resources/commanderImage/placeholder.svg"
+                />
+            );
+        }
+    }
 
     return (
         <>
-            <Row justify={"center"}>
-              balbalbla
+            <Row gutter={[16, 16]}>
+                <Col span={24}></Col>
             </Row>
-            <Row justify={"center"}>
-                <Card/>
-            </Row>
-            <Row justify={"center"}>
-                <Col span={24}>RAMP</Col>
-            </Row>
-            <Row>
-                <Col span={24}>neco</Col>
 
-            </Row>
             <Row>
-                <Col span={6}>col-6</Col>
-                <Col span={6}>col-6</Col>
-                <Col span={6}>col-6</Col>
-                <Col span={6}>col-6</Col>
+                <Col span={2}></Col>
+                <Col span={4}></Col>
+                <Col span={2}>
+                    <ImageDemo />
+                </Col>
+                <Col span={10}>
+                    <Descriptions title={myData.commanderName}>
+                        <Descriptions.Item>{myData.description}</Descriptions.Item>
+                    </Descriptions>
+                </Col>
+                <Col span={6}></Col>
+            </Row>
+            <Divider />
+            <Row>
+                <Col span={6}></Col>
+                <Col span={12}>
+                    <List
+                        itemLayout="horizontal"
+                        dataSource={myData.abilities}
+                        renderItem={(item) => (
+                            <div>
+                                <List.Item>
+                                    <List.Item.Meta
+                                        avatar={
+                                            <div>
+                                                <Avatar
+                                                    src="/resources/commanderImage/placeholder.svg"
+                                                    shape="square"
+                                                    size={64}
+                                                />
+                                                <Badge count={0} showZero offset={[0, -32]}>
+                                                    <a href="#" className="head-example" />
+                                                </Badge>
+                                            </div>
+                                        }
+                                        title={<a href="https://ant.design">{item.name}</a>}
+                                        description={item.description}
+                                    />
+                                </List.Item>
+                            </div>
+                        )}
+                    />
+                </Col>
+                <Divider />
+                <Col span={6}></Col>
             </Row>
         </>
     );
