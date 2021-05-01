e83d89db4b2d9182ba56afda47d2d53cd53fcbd5
Petr Vecera <petr.vecera@profiq.com>
2021-05-01 23:00:22 +0200

    Fix issue with global analysis #32
    https://github.com/petrvecera/coh2ladders/issues/32

--- packages/functions/src/libs/global-stats.ts
+++ packages/functions/src/libs/global-stats.ts
@@ -3,13 +3,13 @@ import { firestore } from "firebase-admin";
 
 const globallyAnalyzedMatches = async (amountOfMatches: number): Promise<void> => {
   const statsDoc = getGlobalStatsDocRef();
-  const increment = firestore.FieldValue.increment(amountOfMatches);
+  const increment = firestore.FieldValue.increment(amountOfMatches || 0);
   await statsDoc.set({ analyzedMatches: increment }, { merge: true });
 };
 
 const globallyAnalyzedTopMatches = async (amountOfMatches: number): Promise<void> => {
   const statsDoc = getGlobalStatsDocRef();
-  const increment = firestore.FieldValue.increment(amountOfMatches);
+  const increment = firestore.FieldValue.increment(amountOfMatches || 0);
   await statsDoc.set({ analyzedTopMatches: increment }, { merge: true });
 };
 export { globallyAnalyzedMatches, globallyAnalyzedTopMatches };
