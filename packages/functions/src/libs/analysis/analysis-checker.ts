a7f71920190795ede12b1103b9a7a8173d225899
Petr Vecera <vecera.petr@gmail.com>
2021-10-02 22:17:13 +0200

    Run analysis aggregation every day

--- packages/functions/src/libs/analysis/analysis-checker.ts
+++ packages/functions/src/libs/analysis/analysis-checker.ts
@@ -1,24 +1,17 @@
-import { getYesterdayDateTimestamp, isMonday } from "../helpers";
+import { getYesterdayDateTimestamp } from "../helpers";
 import { runAndSaveMultiDayAnalysis } from "./multi-day-analysis";
 
 /**
  * This functions checks and runs multi-day analysis
  */
 const analysisChecker = async (): Promise<void> => {
-  const date = new Date();
-  if (isMonday(date)) {
-    await runAndSaveMultiDayAnalysis(new Date(getYesterdayDateTimestamp() * 1000), "week");
-    await runAndSaveMultiDayAnalysis(new Date(getYesterdayDateTimestamp() * 1000), "week", "top");
-  }
+  // We want to run aggregation function regardless fo the day in a week
+  await runAndSaveMultiDayAnalysis(new Date(getYesterdayDateTimestamp() * 1000), "week");
+  await runAndSaveMultiDayAnalysis(new Date(getYesterdayDateTimestamp() * 1000), "week", "top");
 
-  if (date.getDate() === 1) {
-    await runAndSaveMultiDayAnalysis(new Date(getYesterdayDateTimestamp() * 1000), "month");
-    await runAndSaveMultiDayAnalysis(
-      new Date(getYesterdayDateTimestamp() * 1000),
-      "month",
-      "top",
-    );
-  }
+  await runAndSaveMultiDayAnalysis(new Date(getYesterdayDateTimestamp() * 1000), "month", "map");
+  await runAndSaveMultiDayAnalysis(new Date(getYesterdayDateTimestamp() * 1000), "month");
+  await runAndSaveMultiDayAnalysis(new Date(getYesterdayDateTimestamp() * 1000), "month", "top");
 };
 
 export { analysisChecker };
