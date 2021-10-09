81eeadee3071d04e5d851e6a183d7b984a23d48e
Alec Trievel <atrievel@gmail.com>
2021-10-09 00:45:14 -0400

    run Prettier

--- packages/web/src/tests/coh/test-helpers.ts
+++ packages/web/src/tests/coh/test-helpers.ts
@@ -1,13 +1,10 @@
 const getRandomElement = (data: any) => {
   const keys = Object.keys(data);
   return (data as any)[keys[Math.floor(Math.random() * keys.length)]];
-}
+};
 
 const getDataCount = (data: any) => {
   return Object.values(data).length - 1;
-}
-
-export {
-  getRandomElement,
-  getDataCount
 };
+
+export { getRandomElement, getDataCount };
