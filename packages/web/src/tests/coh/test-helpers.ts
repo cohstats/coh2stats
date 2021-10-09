783843f5e3a9ee61d609509bde9c3dfe98cb631e
Alec Trievel <atrievel@gmail.com>
2021-10-08 23:55:04 -0400

    add test helper functions

---
+++ packages/web/src/tests/coh/test-helpers.ts
@@ -0,0 +1,13 @@
+const getRandomElement = (data: any) => {
+  const keys = Object.keys(data);
+  return (data as any)[keys[Math.floor(Math.random() * keys.length)]];
+}
+
+const getDataCount = (data: any) => {
+  return Object.values(data).length - 1;
+}
+
+export {
+  getRandomElement,
+  getDataCount
+};
