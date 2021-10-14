d03de1baa78409010730b9f37e6567ae99cde9c8
Luiz Henrique Pegoraro <pegorarohluiz@gmail.com>
2021-10-14 11:04:03 -0300

    Feat/alertbox  (#96)
    * adds alertbox component
    
    * adds test file and updates alert box to better standards
    
    * adds test scenario
    
    * adds test scenario
    
    Signed-off-by: Luiz Henrique Pegoraro <pegorarohluiz@gmail.com>
    
    * Yarn fix
    
    * Fixes failing test with correct matcher
    
    * Clears lint warnings on alert box component

---
+++ packages/web/src/components/alert-box.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Alert } from "antd";
+
+const defaultCloseable = true;
+
+export interface AlertEvent {
+  /** Type of Alert styles, options:`success`, `info`, `warning`, `error` */
+  type: "success" | "info" | "warning" | "error";
+  /** Content of Alert */
+  message: React.ReactNode | string;
+  /** Additional content of Alert */
+  description?: React.ReactNode | string;
+  /** Whether Alert can be closed */
+  closable?: boolean;
+}
+
+export const AlertBox: React.FC<AlertEvent> = (event: AlertEvent) => {
+  return (
+    <Alert
+      data-testid="alert-box"
+      message={event.message}
+      type={event.type}
+      description={event.description}
+      closable={event.closable ?? defaultCloseable}
+    />
+  );
+};
