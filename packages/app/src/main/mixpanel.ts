efdbcdf8186e0ffb1b5019a89f64386c02f06383
Petr Vecera <petr.vecera@profiq.com>
2022-02-19 18:47:23 +0100

    Add mixpanel events

---
+++ packages/app/src/main/mixpanel.ts
@@ -0,0 +1,42 @@
+import { init } from "mixpanel";
+import config from "./config";
+import { machineIdSync } from "node-machine-id";
+import { app } from "electron";
+
+const mixpanel = init(config.mixpanelProjectToken);
+
+// Generates anonymous unique ID / machine, so we can distinguish between users
+const clientId = machineIdSync(true);
+
+const events = {
+  init: (): void => {
+    mixpanel.track("app_init", {
+      distinct_id: clientId,
+      version: app.getVersion(),
+    });
+  },
+  new_match_found: (map: string): void => {
+    mixpanel.track("new_match_found", {
+      distinct_id: clientId,
+      map: map,
+    });
+  },
+  about_open: (): void => {
+    mixpanel.track("about_open", {
+      distinct_id: clientId,
+    });
+  },
+  settings_open: (): void => {
+    mixpanel.track("settings_open", {
+      distinct_id: clientId,
+    });
+  },
+  click_on_player: (type: string): void => {
+    mixpanel.track("open_player_profile", {
+      distinct_id: clientId,
+      type: type,
+    });
+  },
+};
+
+export { events };
