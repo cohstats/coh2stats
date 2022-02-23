11a82d22c0d02b7b44448f52b40e506c28454b34
Petr Vecera <petr.vecera@profiq.com>
2022-02-23 23:52:12 +0100

    Add titlebar library

--- packages/app/src/renderer/windows/preload.tsx
+++ packages/app/src/renderer/windows/preload.tsx
@@ -1,7 +1,14 @@
 import { contextBridge, ipcRenderer, shell } from "electron";
 import { configureRendererStore } from "../../redux/configureStoreRenderer";
 
+import { Titlebar } from "custom-electron-titlebar";
+
 const store = configureRendererStore();
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Title bar implemenation
+  new Titlebar();
+});
 
 contextBridge.exposeInMainWorld("electron", {
   store: store,
