ace0595f3fe1b9c4ee62d4e29b0e69da0d967959
Petr Vecera <petr.vecera@profiq.com>
2022-08-10 23:30:32 +0200

    Remove null check on data it's not needed there

--- packages/web/src/pages/players/player-card.tsx
+++ packages/web/src/pages/players/player-card.tsx
@@ -150,10 +150,10 @@ const PlayerCard = () => {
     });
   };
 
-  const steamProfile = data?.steamProfile ? data?.steamProfile[steamid] : null;
-  const playTime = data?.playTime ? Math.floor(data?.playTime / 60) : null;
+  const steamProfile = data.steamProfile ? data.steamProfile[steamid] : null;
+  const playTime = data.playTime ? Math.floor(data.playTime / 60) : null;
 
-  const relicData = data?.relicPersonalStats;
+  const relicData = data.relicPersonalStats;
   const statGroups = relicData.statGroups;
   const playerRelicProfile = findPlayerProfile(statGroups);
 
