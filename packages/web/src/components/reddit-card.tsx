fe1f6316d966cbda1c87986c508c8e15d5680426
Malay <malaykumar2003@gmail.com>
2022-10-02 10:58:03 +0530

    Fix: used fetch instead of axios

--- packages/web/src/components/reddit-card.tsx
+++ packages/web/src/components/reddit-card.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 import RedditCardDetails from "./reddit-card-details";
 import { useState, useEffect } from "react";
 import { Card, Typography } from "antd";
@@ -12,10 +11,14 @@ const RedditCard: React.FC<Props> = (size) => {
   const [postData, setpostData] = useState([]);
   const [fetched, setFetched] = useState(false);
   const data = async () => {
-    const res = await axios.get(
-      "https://www.reddit.com/r/CompanyOfHeroes/top.json?limit=50&t=week",
-    );
-    setpostData(res?.data?.data?.children);
+    const res = await fetch("https://www.reddit.com/r/CompanyOfHeroes/top.json?limit=50&t=week");
+    const resData = await res.json();
+    const requierdData = resData?.data?.children
+      .filter(function (e: any) {
+        return e?.data?.link_flair_text === "CoH2";
+      })
+      .slice(0, 10);
+    setpostData(requierdData);
     setFetched(true);
     return postData;
   };
@@ -29,26 +32,21 @@ const RedditCard: React.FC<Props> = (size) => {
         <div style={{ overflow: "hidden" }}>
           <Title level={3}>Top Reddit Posts</Title>
           <p>r/CompanyOfHeros</p>
-          {postData
-            .filter(function (e: any) {
-              return e?.data?.link_flair_text === "CoH2";
-            })
-            .slice(0, 10)
-            .map((item: any, index: number) => {
-              return (
-                item && (
-                  <RedditCardDetails
-                    key={index}
-                    title={item?.data?.title}
-                    ups={item?.data?.ups}
-                    author={item?.data?.author}
-                    imgs={item?.data?.url_overridden_by_dest}
-                    created={item?.data?.created}
-                    permalink={item?.data.permalink}
-                  ></RedditCardDetails>
-                )
-              );
-            })}
+          {postData.map((item: any, index: number) => {
+            return (
+              item && (
+                <RedditCardDetails
+                  key={index}
+                  title={item?.data?.title}
+                  ups={item?.data?.ups}
+                  author={item?.data?.author}
+                  imgs={item?.data?.url_overridden_by_dest}
+                  created={item?.data?.created}
+                  permalink={item?.data.permalink}
+                ></RedditCardDetails>
+              )
+            );
+          })}
         </div>
       </Card>
     </div>
