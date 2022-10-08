1583c4e3f8e8d7dc612840f1c045e51a6b546658
Manthan Kuber <manthankuber873@gmail.com>
2022-10-08 09:48:38 +0530

    feat: update regex and set default values from api

--- packages/web/src/pages/profile/user-profile.tsx
+++ packages/web/src/pages/profile/user-profile.tsx
@@ -1,5 +1,5 @@
 import { Col, Row, Form, Input, Button } from "antd";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 
 const inputList = [
@@ -16,8 +16,7 @@ const inputList = [
   },
   {
     label: "Reddit Profile",
-    pattern:
-      /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/, // Update Later
+    pattern: /http(?:s)?:\/\/(?:www\.)?reddit\.com\/user\/([a-zA-Z0-9_]+)/,
     name: "redditProfile",
   },
   {
@@ -27,13 +26,23 @@ const inputList = [
   },
   {
     label: "CoH 2 Org Profile",
-    pattern:
-      /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/, // Update Later
+    pattern: /http(?:s)?:\/\/(?:www\.)?coh2stats\.com\/profile\/([a-zA-Z0-9_]+)/,
     name: "coh2orgProfile",
   },
 ];
 
+type FormValues = {
+  twitchProfile: string;
+  youtubeProfile: string;
+  redditProfile: string;
+  twitterProfile: string;
+  coh2orgProfile: string;
+};
+
 const UserProfile: React.FC = () => {
+  const [userProfileForm] = Form.useForm();
+  const [isDisabled, setIsDisabled] = useState(true);
+
   useEffect(() => {
     // THIS LOADS THE DAT FROM THE DATABASE
     (async () => {
@@ -42,15 +51,18 @@ const UserProfile: React.FC = () => {
 
       if (docSnap.exists()) {
         console.log("Document data:", docSnap.data());
+        userProfileForm.setFieldsValue(docSnap.data());
+        setIsDisabled(false);
       } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
+        setIsDisabled(false); 
       }
     })();
   }, []);
 
   // Maybe use as useEffect? It's up to you
-  const saveProfile = async ({ twitchProfile = "whatever", youtubeProfile = "aasdfasd" }) => {
+  const saveProfile = async ({ twitchProfile, youtubeProfile }: FormValues) => {
     // THIS SAVES THE DATA
     const userProfileRef = doc(getFirestore(), "test/mockProfile");
     await setDoc(userProfileRef, { twitchProfile, youtubeProfile }, { merge: true });
@@ -58,7 +70,7 @@ const UserProfile: React.FC = () => {
     await setDoc(userProfileRef, { youtubeProfile }, { merge: true });
   };
 
-  const onFinish = (values: any) => {
+  const onFinish = (values: FormValues) => {
     console.log("Success:", values);
     //save profile here
   };
@@ -72,27 +84,26 @@ const UserProfile: React.FC = () => {
       <Row justify="center" style={{ paddingTop: "20px" }}>
         <Col xs={23} xxl={17}>
           <Form
+            form={userProfileForm}
             name="userProfileForm"
             labelCol={{ span: 8 }}
             wrapperCol={{ span: 16 }}
-            initialValues={{ remember: true }}
             onFinish={onFinish}
             onFinishFailed={onFinishFailed}
             autoComplete="off"
           >
             {inputList.map((item) => (
               <Form.Item
+                key={item.label}
                 label={`${item.label}`}
                 name={`${item.name}`}
-                rules={[
-                  { message: `Please enter a valid ${item.label}!`, pattern: item.pattern },
-                ]}
+                rules={[{ message: `${item.label} should be valid!`, pattern: item.pattern }]}
               >
-                <Input />
+                <Input disabled={isDisabled} />
               </Form.Item>
             ))}
             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
+              <Button type="primary" htmlType="submit" disabled={isDisabled}>
                 Submit
               </Button>
             </Form.Item>
