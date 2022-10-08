7f752b2ba63aeaba380bc01e5a2a00d1aba037f9
Manthan Kuber <67104856+Manthan-Kuber@users.noreply.github.com>
2022-10-08 16:54:27 +0530

    Feat: Add user profile form (#271)
    * feat: add antd form and inputlist
    
    * feat: update regex and set default values from api
    
    * feat: update form submit method

--- packages/web/src/pages/profile/user-profile.tsx
+++ packages/web/src/pages/profile/user-profile.tsx
@@ -1,8 +1,56 @@
-import { Col, Row } from "antd";
-import React, { useEffect } from "react";
+import { Col, Row, Form, Input, Button } from "antd";
+import React, { useEffect, useState } from "react";
 import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 
+const inputList = [
+  {
+    label: "Twitch Profile",
+    pattern: /^(?:https?:\/\/)?(?:www\.|go\.)?twitch\.tv\/([a-z0-9_]+)($|\?)/,
+    name: "twitchProfile",
+  },
+  {
+    label: "Youtube Profile",
+    pattern:
+      /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/,
+    name: "youtubeProfile",
+  },
+  {
+    label: "Reddit Profile",
+    pattern: /http(?:s)?:\/\/(?:www\.)?reddit\.com\/user\/([a-zA-Z0-9_]+)/,
+    name: "redditProfile",
+  },
+  {
+    label: "Twitter Profile",
+    pattern: /http(?:s)?:\/\/(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/,
+    name: "twitterProfile",
+  },
+  {
+    label: "CoH 2 Org Profile",
+    pattern: /http(?:s)?:\/\/(?:www\.)?coh2stats\.com\/profile\/([a-zA-Z0-9_]+)/,
+    name: "coh2orgProfile",
+  },
+];
+
+type FormValues = {
+  twitchProfile: string;
+  youtubeProfile: string;
+  redditProfile: string;
+  twitterProfile: string;
+  coh2orgProfile: string;
+};
+
+const initialFormValues = {
+  twitchProfile: "",
+  youtubeProfile: "",
+  redditProfile: "",
+  twitterProfile: "",
+  coh2orgProfile: "",
+};
+
 const UserProfile: React.FC = () => {
+  const [userProfileForm] = Form.useForm();
+  const [isDisabled, setIsDisabled] = useState(true); //Disabled form fields and button while loading
+
   useEffect(() => {
     // THIS LOADS THE DAT FROM THE DATABASE
     (async () => {
@@ -11,27 +59,70 @@ const UserProfile: React.FC = () => {
 
       if (docSnap.exists()) {
         console.log("Document data:", docSnap.data());
+        userProfileForm.setFieldsValue(docSnap.data()); // Set inital form values (default being "")
+        setIsDisabled(false); //Allow user to edit after values are fetched
       } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
+        setIsDisabled(false); //If no initial data exists, set disabled to false
       }
     })();
   }, []);
 
   // Maybe use as useEffect? It's up to you
-  const saveProfile = async ({ twitchProfile = "whatever", youtubeProfile = "aasdfasd" }) => {
+  const saveProfile = async ({
+    twitchProfile,
+    youtubeProfile,
+    redditProfile,
+    twitterProfile,
+    coh2orgProfile,
+  }: FormValues) => {
     // THIS SAVES THE DATA
     const userProfileRef = doc(getFirestore(), "test/mockProfile");
-    await setDoc(userProfileRef, { twitchProfile, youtubeProfile }, { merge: true });
-    // You can also send just one field to be updated
-    await setDoc(userProfileRef, { youtubeProfile }, { merge: true });
+    await setDoc(
+      userProfileRef,
+      { twitchProfile, youtubeProfile, redditProfile, twitterProfile, coh2orgProfile },
+      { merge: true },
+    );
+  };
+
+  const onFinish = (values: FormValues) => {
+    console.log("Success:", values);
+    setIsDisabled(true); //Disable form fields while data is being saved in the backend
+    saveProfile(values);
+    setIsDisabled(false);
   };
 
   return (
     <>
       <Row justify="center" style={{ paddingTop: "20px" }}>
         <Col xs={23} xxl={17}>
-          USER PROFILE
+          <Form
+            form={userProfileForm}
+            name="userProfileForm"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ ...initialFormValues }}
+            onFinish={onFinish}
+            onFinishFailed={(errorInfo: any) => console.log("Failed:", errorInfo)}
+            autoComplete="off"
+          >
+            {inputList.map((item) => (
+              <Form.Item
+                key={item.label}
+                label={`${item.label}`}
+                name={`${item.name}`}
+                rules={[{ message: `${item.label} should be valid!`, pattern: item.pattern }]}
+              >
+                <Input disabled={isDisabled} placeholder={`Enter your ${item.label}`} />
+              </Form.Item>
+            ))}
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit" disabled={isDisabled}>
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
         </Col>
       </Row>
     </>
