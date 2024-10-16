85abdb2a7a25a6800184a01206b9f0ec3c91c00d
Kum yew <lokekumyew@gmail.com>
2024-10-16 11:49:47 +0700

    Removed extra login route in root

--- packages/web/src/root.tsx
+++ packages/web/src/root.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { ConnectedRouter } from "connected-react-router";
 import { Provider } from "react-redux";
-import { Switch, Route } from "react-router-dom";
-import { CompatRoute, CompatRouter } from "react-router-dom-v5-compat";
+// import { Switch, Route } from "react-router-dom";
+// import { CompatRoute, CompatRouter } from "react-router-dom-v5-compat";
 
 // Subcomponents
 import App from "./App";
@@ -10,7 +10,7 @@ import App from "./App";
 // Types
 import type { AppStore } from "./store";
 import type { History } from "history";
-import { ConfigsProvider } from "./config-context";
+// import { ConfigsProvider } from "./config-context";
 
 interface Props {
   store: AppStore;
@@ -20,18 +20,7 @@ const Root: React.FC<Props> = ({ store, history }) => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <React.StrictMode>
-        <Switch>
-          <CompatRoute path="/login">LOGIN</CompatRoute>
-          <Route path="/">
-            <ConfigsProvider configJson={{}}>
-              <App />
-            </ConfigsProvider>
-          </Route>
-          {/* <CompatRoute path="/" configJson={{}}>
-            <App />
-          </CompatRoute> */}
-          <CompatRoute>NOT FOUND</CompatRoute>
-        </Switch>
+        <App />
       </React.StrictMode>
     </ConnectedRouter>
   </Provider>
