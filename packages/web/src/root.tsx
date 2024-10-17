d9de851a6654f150539ea9102d9e7a471f9ed074
Petr Vecera <vecera.petr@gmail.com>
2024-10-17 12:45:00 +0200

    Add config back in

--- packages/web/src/root.tsx
+++ packages/web/src/root.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { ConnectedRouter } from "connected-react-router";
 import { Provider } from "react-redux";
-// import { Switch, Route } from "react-router-dom";
-// import { CompatRoute, CompatRouter } from "react-router-dom-v5-compat";
 
 // Subcomponents
 import App from "./App";
@@ -10,7 +8,7 @@ import App from "./App";
 // Types
 import type { AppStore } from "./store";
 import type { History } from "history";
-// import { ConfigsProvider } from "./config-context";
+import { ConfigsProvider } from "./config-context";
 
 interface Props {
   store: AppStore;
@@ -20,7 +18,9 @@ const Root: React.FC<Props> = ({ store, history }) => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <React.StrictMode>
-        <App />
+        <ConfigsProvider configJson={{}}>
+           <App />
+          </ConfigsProvider>
       </React.StrictMode>
     </ConnectedRouter>
   </Provider>
