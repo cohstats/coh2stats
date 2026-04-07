87a3d1bac44fadf3a12e853ba0a2e3d6c70014e5
Petr Vecera <vecera.petr@gmail.com>
2026-04-07 19:11:40 +0200

    About

--- packages/app/src/renderer/components/about/about.tsx
+++ packages/app/src/renderer/components/about/about.tsx
@@ -12,6 +12,7 @@ import { KofiDonate } from "../donate/kofi-donate";
 import compareVersions from "compare-versions";
 import { useSelector } from "react-redux";
 import { selectSettings } from "../../../redux/slice";
+import config from "../../../main/config";
 
 interface Props {
   modalState: boolean;
@@ -95,11 +96,7 @@ const AboutModal: React.FC<Props> = ({ modalState, setModalState }) => {
               <Text>
                 Want to help?{" "}
                 <Link
-                  onClick={() =>
-                    window.electron.ipcRenderer.openInBrowser(
-                      "https://github.com/cohstats/coh2stats/issues",
-                    )
-                  }
+                  onClick={() => window.electron.ipcRenderer.openInBrowser(config.githubIssuesURL)}
                 >
                   Report a bug
                 </Link>
