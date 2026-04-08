4ada9cf10f0169b0be594b162526f248bba7dd21
Petr Vecera <vecera.petr@gmail.com>
2026-04-08 14:50:16 +0200

    Remove un-used packages

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -18,19 +18,6 @@ const nextConfig = {
   images: {
     unoptimized: false,
   },
-
-  // Webpack configuration
-  webpack: (config, { webpack }) => {
-    // Ignore old page components during migration
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp:
-          /^(react-router|react-router-dom|react-router-dom-v5-compat|connected-react-router|redux|react-redux)$/,
-      }),
-    );
-
-    return config;
-  },
 };
 
 export default nextConfig;
