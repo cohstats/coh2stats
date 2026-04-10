9f13a99abca2862bfd1e16851a15fc21fda554fe
Petr Vecera <vecera.petr@gmail.com>
2026-04-10 17:39:46 +0200

    Force the stats to be light theme

--- packages/web/src/app/map-stats/page.tsx
+++ packages/web/src/app/map-stats/page.tsx
@@ -1,9 +1,25 @@
 "use client";
 
 import MapStats from "./_components/map-stats";
+import { ThemeProvider } from "next-themes";
+import { ConfigProvider, theme } from "antd";
 
 const MapStatsPage = () => {
-  return <MapStats />;
+  return (
+    <ThemeProvider attribute="data-theme" forcedTheme="light">
+      <ConfigProvider theme={{ algorithm: theme.defaultAlgorithm }}>
+        <div
+          style={{
+            backgroundColor: "#f0f2f5",
+            color: "rgba(0, 0, 0, 0.88)",
+            minHeight: "100vh",
+          }}
+        >
+          <MapStats />
+        </div>
+      </ConfigProvider>
+    </ThemeProvider>
+  );
 };
 
 export default MapStatsPage;
--- packages/web/src/app/stats/page.tsx
+++ packages/web/src/app/stats/page.tsx
@@ -1,9 +1,25 @@
 "use client";
 
 import CustomStats from "./_components/custom-stats";
+import { ThemeProvider } from "next-themes";
+import { ConfigProvider, theme } from "antd";
 
 const StatsPage = () => {
-  return <CustomStats />;
+  return (
+    <ThemeProvider attribute="data-theme" forcedTheme="light">
+      <ConfigProvider theme={{ algorithm: theme.defaultAlgorithm }}>
+        <div
+          style={{
+            backgroundColor: "#f0f2f5",
+            color: "rgba(0, 0, 0, 0.88)",
+            minHeight: "100vh",
+          }}
+        >
+          <CustomStats />
+        </div>
+      </ConfigProvider>
+    </ThemeProvider>
+  );
 };
 
 export default StatsPage;
