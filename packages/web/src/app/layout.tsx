76d357faffb7b6c88513e0b621cbf31b9acb7f7b
Petr Vecera <vecera.petr@gmail.com>
2026-04-11 13:55:41 +0200

    hydratation

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -65,7 +65,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#001529" />
