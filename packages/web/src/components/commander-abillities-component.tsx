094d30596278445dbdd3a96765308f97f7fa993a
Petr Vecera <vecera.petr@gmail.com>
2026-04-20 17:14:22 +0200

    Load the images as the commander abilities

--- packages/web/src/components/commander-abillities-component.tsx
+++ packages/web/src/components/commander-abillities-component.tsx
@@ -1,12 +1,13 @@
 import { CommanderAbility } from "../coh/types";
-import { Avatar, Badge, Col, Row, Tooltip, Typography } from "antd";
+import { Badge, Col, Row, Tooltip, Typography } from "antd";
 import { getExportedIconPath } from "../coh/helpers";
 import React from "react";
+import Image from "next/image";
 
 const { Text } = Typography;
 
-const OFFSET_DEFAULT: [number, number] = [-16, -32];
-const OFFSET_SMALL: [number, number] = [-12, -24];
+const OFFSET_DEFAULT: [number, number] = [-4, 7];
+const OFFSET_SMALL: [number, number] = [-4, 7];
 
 type CommanderAbilityProps = {
   commanderAbilities: Array<CommanderAbility>;
@@ -26,24 +27,33 @@ export const CommanderAbilitiesComponent = ({
           <Text>{commanderDescription}</Text>
         </Row>
       )}
-      <Row>
+      <Row gutter={20}>
         {commanderAbilities.map((item: CommanderAbility) => {
+          const size = isSmall ? 48 : 64;
           return (
-            <Col key={item.name}>
+            <Col key={item.name} >
               <Tooltip placement={"bottom"} title={item.description}>
-                <Avatar
-                  alt={item.name}
-                  src={getExportedIconPath(item.icon)}
-                  shape="square"
-                  size={isSmall ? 48 : 64}
-                />
-                <Badge
-                  count={item.commandPoints}
-                  overflowCount={999}
-                  showZero
-                  offset={isSmall ? OFFSET_SMALL : OFFSET_DEFAULT}
-                  size={isSmall ? "small" : "medium"}
-                />
+                <>
+                  <Badge
+                    count={item.commandPoints}
+                    overflowCount={999}
+                    showZero
+                    offset={isSmall ? OFFSET_SMALL : OFFSET_DEFAULT}
+                    size={isSmall ? "small" : "medium"}
+                  >
+                    <Image
+                      alt={item.name}
+                      src={getExportedIconPath(item.icon)}
+                      width={size}
+                      height={size}
+                      style={{
+                        display: "block",
+                        borderRadius: 0,
+                      }}
+                    />
+                  </Badge>
+
+                </>
               </Tooltip>
             </Col>
           );
