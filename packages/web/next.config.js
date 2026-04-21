c6f6dafaf979ae78453a8e66b5506938ad046f66
Petr Vecera <vecera.petr@gmail.com>
2026-04-21 20:39:15 +0200

    Small cahnges

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -21,4 +21,9 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
--- packages/web/src/firebase/firebase.ts
+++ packages/web/src/firebase/firebase.ts
@@ -6,22 +6,12 @@ import {
   setUserId as setFirebaseUserID,
 } from "firebase/analytics";
 import { FirebaseApp, initializeApp } from "firebase/app";
-import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
-import { getPerformance } from "firebase/performance";
-import { getFirestore, connectFirestoreEmulator, Firestore } from "firebase/firestore";
 
 // Local
 import config from "../config";
 
-let performance;
 let app: FirebaseApp | undefined;
 let analytics: Analytics | undefined;
-let db: Firestore | undefined;
-
-const useEmulators =
-  (process.env.NEXT_PUBLIC_EMULATOR || process.env.REACT_APP_EMULATOR) &&
-  process.env.NEXT_PUBLIC_EMULATOR !== "false" &&
-  process.env.REACT_APP_EMULATOR !== "false";
 
 /**
  * Initialize Firebase
@@ -34,16 +24,8 @@ const init = (): void => {
   try{
     app = initializeApp(config.firebase());
     analytics = getAnalytics(app);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    performance = getPerformance(app);
-    db = getFirestore(app);
-
     setUserProperties(analytics, { custom_platform: "web_app" });
 
-    if (useEmulators) {
-      connectFirestoreEmulator(db, "localhost", 8080);
-      connectFunctionsEmulator(functions(), "localhost", 5001);
-    }
   } catch (e) {
     console.error("Firebase initialization error", e);
     return;
@@ -54,12 +36,6 @@ const init = (): void => {
 if (typeof window !== "undefined") {
   init();
 }
-
-/**
- * Instance of the FB functions
- */
-export const functions = () =>
-  getFunctions(app, useEmulators ? undefined : config.firebaseFunctions.location);
 
 /**
  * Log analytics event
@@ -102,27 +78,13 @@ const setUserId = (id: string): void => {
  */
 const resetUserId = (): void => setUserId("");
 
-/**
- * Get Firestore instance
- * @returns Firestore instance or undefined if not initialized
- */
-const getDb = (): Firestore | undefined => db;
-
-/**
- * Get Firebase App instance
- * @returns Firebase App instance or undefined if not initialized
- */
-const getApp = (): FirebaseApp | undefined => app;
-
 const firebaseExport = {
   init,
   app,
-  functions,
   logEvent,
   setUserId,
   resetUserId,
-  getDb,
-  getApp,
+
 };
 
 export default firebaseExport;
