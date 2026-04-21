3b4b8c5e4c25dab591f6520746cfd313a720d259
Petr Vecera <vecera.petr@gmail.com>
2026-04-21 17:34:36 +0200

    Suspense

--- packages/web/src/app/search/page.tsx
+++ packages/web/src/app/search/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { Suspense } from "react";
 import CustomSearch from "../../components/search/search";
 
 const SearchPage = () => {
-  return <CustomSearch />;
+  return (
+    <Suspense fallback={<div style={{textAlign: "center" }}>Loading...</div>}>
+      <CustomSearch />
+    </Suspense>
+  );
 };
 
 export default SearchPage;
