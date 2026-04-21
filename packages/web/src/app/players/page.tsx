28f4bade85b8f64865217aff01e0fa0e80d1d69a
Petr Vecera <vecera.petr@gmail.com>
2026-04-21 18:00:36 +0200

    Custom search suspense

--- packages/web/src/app/players/page.tsx
+++ packages/web/src/app/players/page.tsx
@@ -2,6 +2,7 @@ import CustomSearch from "../../components/search/search";
 import PlayerStats from "./_components/player-stats";
 import { Col, Row } from "antd";
 import { getPlayerStats } from "@/firebase/firebase-server";
+import { Suspense } from "react";
 
 // ISR configuration: revalidate every 12 hours (43200 seconds)
 export const revalidate = 43200;
@@ -13,7 +14,9 @@ const PlayersPage = async () => {
   return (
     <>
       <div style={{ paddingTop: 10 }}>
-        <CustomSearch />
+        <Suspense fallback={<div style={{ textAlign: "center" }}>Loading...</div>}>
+          <CustomSearch />
+        </Suspense>
       </div>
       <Row justify="center" style={{ minHeight: "600px" }}>
         <Col xs={23} md={22} xxl={15}>
