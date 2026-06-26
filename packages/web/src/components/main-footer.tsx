3d623bdaa1b2e6958d89b398cbb87b3203b0c558
Petr Vecera <vecera.petr@gmail.com>
2026-06-26 20:46:20 +0200

    Footer changes

--- packages/web/src/components/main-footer.tsx
+++ packages/web/src/components/main-footer.tsx
@@ -12,57 +12,62 @@ const { Link, Text } = Typography;
 
 export const MainFooter: React.FC = () => {
   return (
-    <Footer style={{ textAlign: "center", padding: 0, paddingBottom: 10, marginTop: 20 }}>
+    <Footer style={{ padding: "20px 20px", marginTop: 20 }}>
       <Divider style={{ marginTop: 0 }} />
-      This is unofficial fan-made site for Company Of Heroes 2. Not associated with Relic
-      Entertainment.
-      <br />
-      The data display here are not accurate and do not represent the actual state of the game.
-      <br />
-      See{" "}
-      <NextLink href={"/about"} prefetch={false}>
-        about page
-      </NextLink>{" "}
-      for more info.
-      <br />
-      © 2021 - 2026 COH2stats.com
-      <br />
-      <Link
-        href={"https://github.com/cohstats/coh2stats"}
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{ marginRight: 15 }}
-      >
-        <Image src={githubIcon} width={30} height={30} alt="GitHub Logo" />
-      </Link>
-      <Link
-        href={config.discordInviteLink}
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{ marginRight: 15 }}
-      >
-        <Image src={discordIcon} width={30} height={30} alt="Discord Logo" />
-      </Link>
-      <Link href={config.donationLink} target="_blank" rel="noopener noreferrer" strong>
-        <div style={{ display: "inline-flex", alignItems: "center", gap: "6px" }}>
-          <Image src={kofiLogo} width={30} height={30} alt="Ko-fi support button" />
-          <span>Donate</span>
+
+      {/* Container with max width */}
+      <div style={{ maxWidth: 1200, margin: "0 auto" }}>
+        {/* Top section: Left text + Right buttons */}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "flex-start",
+            marginBottom: 20,
+          }}
+        >
+          {/* Left side - Copyright and unofficial text */}
+          <div style={{ textAlign: "left", maxWidth: "60%" }}>
+            <div>© 2021 - 2026 COH2stats.com</div>
+            <div>This is an unofficial fan-made site for Company Of Heroes 2.</div>
+          </div>
+
+          {/* Right side - Social buttons */}
+          <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+            <Link href={config.discordInviteLink} target="_blank" rel="noopener noreferrer">
+              <Image src={discordIcon} width={30} height={30} alt="Discord Logo" />
+            </Link>
+            <Link
+              href={"https://github.com/cohstats/coh2stats"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image src={githubIcon} width={30} height={30} alt="GitHub Logo" />
+            </Link>
+            <Link href={config.donationLink} target="_blank" rel="noopener noreferrer">
+              <Image src={kofiLogo} width={30} height={30} alt="Ko-fi support button" />
+            </Link>
+          </div>
         </div>
-      </Link>
-      <br />
-      <br />
-      <Text type="secondary" style={{ fontSize: "12px" }}>
-        The Company of Heroes is registered trademark of SEGA Holdings. Co
-        <br />
-        The COH2 Images and other assets are owned by Relic Entertainment and/or SEGA
-      </Text>
-      <br />
-      <br />
-      Visit{" "}
-      <Link href={"https://coh3stats.com"} target="_blank" rel="noopener noreferrer">
-        coh3stats.com
-      </Link>{" "}
-      for Company of Heroes 3 stats and analytics
+
+        {/* Bottom section - Centered trademark and links */}
+        <div style={{ textAlign: "center" }}>
+          <Text type="secondary" style={{ fontSize: "12px" }}>
+            The Company of Heroes is a registered trademark of Relic Entertainment.
+            <br />
+            The COH Images and other assets are owned by Relic Entertainment.
+          </Text>
+          <br />
+          <br />
+          <div>
+            Visit{" "}
+            <Link href={"https://coh3stats.com"} target="_blank" rel="noopener noreferrer">
+              coh3stats.com
+            </Link>{" "}
+            for Company of Heroes 3 stats and analytics
+          </div>
+        </div>
+      </div>
     </Footer>
   );
 };
